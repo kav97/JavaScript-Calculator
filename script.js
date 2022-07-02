ccc239e65c54492650715862dffcdc7ad4b69d13
Kavandeep <kavan1@live.co.uk>
2022-07-02 18:38:15 +0100

    setting up initial variables

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-//number button
-//button pressed,log in the console,
-//shown in the display
-//querySelectorAll .class
-//store the first number before click on operate button
-//store the operator
-//start the second number
-//click =  store  second number
-//calculate get result shown in display
+// define initial vals and variables
+const numberButton = document.querySelectorAll(".btn__btn-num, .btn__btn-num-0")
+const opperatorButton = document.querySelectorAll(".btn__btn-opperator")
+const equalsTo = document.querySelectorAll("#equal")
+const screenDisplay = document.querySelectorAll(".main__calculation-textbox");
+const allClear = document.querySelectorAll("#keypad-clear")
 
-const getNumber = document.querySelectorAll(".btn__btn-num, .btn__btn-num-0")
-const getOperator = document.querySelectorAll(".btn__btn-opperator")
-const getEquals = document.querySelector("#equal")
-const getScreenDisplay = document.querySelector(".main__calculation-textbox");
-const getAllClear = document.querySelector("#keypad-clear")
+// define the functions needed to carry out all the problems
 
-let num1 = ""
-let num2 = ""
-let num3 = ""
-
-
-function calcFunction() {
-    if (num2 == "*") {
-        getScreenDisplay.value = num1 * num3;
-    } else if (num2 == "/") {
-        getScreenDisplay.value = num1 / num3;
-    } else if (num2 == "-") {
-        getScreenDisplay.value = num1 - num3;
-    } else if (num2 == "+") {
-        getScreenDisplay.value = num1 + num3;
-    } 
-    num2 = ""
-}
+// add event triggers that reads input, performs the action and returns an output
